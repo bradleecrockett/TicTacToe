e6f69a2ddfff8b26cbce3365575cb9ec1b53d0f3
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2020-10-19 19:28:36 -0700

    Add files via upload

---
+++ ttt.py
@@ -0,0 +1,35 @@
+# Name:
+# Date:
+# Period:
+# Program: Tic Tac Toe game
+
+###### Rubric
+# Program Requirement                                       | Points Possible   | Points Earned
+# -----|:-----:|:-----:
+# Use a list to store the state of the game                 | 5                 |
+# Properly check for a horizontal winner                    | 3                 |
+# Properly check for a vertical winner                      | 3                 |
+# Properly check for a diagonal winner                      | 2                 |
+# Users can select cells to place an X or O respectively    | 4                 |
+# User can continue to select cells as long as needed       | 2                 |
+# Users can only select cells that are available            | 1                 |
+# Game ends appropriately, X wins, O wins or Cats Game      | 2                 |
+# Style, variable names, spacing, main() etc.               | 6                 |
+# Header                                                    | 2                 |
+# Total                                                     | 30                |
+
+
+
+def main():
+    print("Welcome to Tic Tac Toe!")
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
